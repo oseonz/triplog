27fc3dc40632d9e65c954ecf9067c8d041f10c61
Steave Inhun Ko <yoimono@naver.com>
2025-06-01 17:52:11 +0900

    기사목록

--- src/pages/info/ArticlesPage.jsx
+++ src/pages/info/ArticlesPage.jsx
@@ -2,36 +2,59 @@ import React, { useEffect, useState } from "react";
 import NewsCard from "../../components/info/ArticlesCard";
 import ArticlesCard from "../../components/info/ArticlesCard";
 import axios from "axios";
+import { set } from "date-fns";
 
 
 
 
 function ArticlesPage() {
 
+const [page, setPage] = useState(1); // 네이버 API는 페이지가 1부터 시작하므로 1로 설정
+const [keyword, setKeyword] = useState("국내여행가볼곳"); 
+const pageSize = 8; // 페이지당 기사 수
+const [totalElements, setTotalElements] = useState(0);
+const [totalPage, setTotalPage] = useState(0);
 
-const [keyword, setKeyword] = useState("추천여행지"); 
-const page = 1; // 네이버 API는 페이지가 1부터 시작하므로 1로 설정
-const pageSize = 10; // 페이지당 기사 수
 
-const API_URL = `http://localhost:8081/api/news?keyword=${keyword}&page=${page}&size=${pageSize}`;
 
 const [articleList, setArticleList] = useState([]);
 
-useEffect(()=>{
-  async function fetchData() {
-    try {
-      const rest = await axios(API_URL);
-      console.log(rest.data.items);
-      setArticleList(rest.data.items);
-    }
-    catch (error) {
-      console.error("데이터를 가져오는 중 오류 발생:", error);
-    }
-  }
+function handlePageChange(newPage) {
+  setPage(newPage);
+}
 
+async function fetchData() {
+  try {
+
+    const API_URL = `http://localhost:8081/api/news?keyword=${keyword}&page=${page}&size=${pageSize}`;
+
+    const res = await axios(API_URL);
+    console.log(res);
+
+    setTotalElements(res.data.total); // 전체 아이템 수
+
+    console.log("전체 아이템 수:", totalElements);
+    setArticleList(res.data.items);
+
+
+    const totalPages = Math.ceil(res.data.total / pageSize); // // 전체 페이지 수 계산
+    setTotalPage(totalPages);
+
+    console.log("전체 페이지 수:", totalPage);
+  }
+  catch (error) {
+    console.error("데이터를 가져오는 중 오류 발생:", error);
+  }
+}
+
+useEffect(()=>{
   fetchData();
 },[]);
 
+
+useEffect(() => {
+  fetchData();
+},[page]);
 
   return (
     <>
@@ -45,13 +68,12 @@ useEffect(()=>{
       </div>
 
 
-            <div className="min-h-screen bg-[#F3F5F6] flex justify-center pt-[72px]">
+      <div className="min-h-screen bg-[#F3F5F6] flex justify-center pt-[52px]">
         <div className="container">
           <div className="flex justify-center">
-            <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-2 gap-6 w-full max-w-6xl">
+            <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 w-full max-w-6xl">
 
               {articleList.map((article, index)=>{
-
                   return (
                   <ArticlesCard
                       key={index}
@@ -66,6 +88,27 @@ useEffect(()=>{
             </div>
           </div>
         </div>
+      </div>
+            <div className="flex justify-center items-center mt-4 gap-3">
+        <button
+          className="px-2 py-1 text-sm text-white bg-blue-400 rounded"
+          onClick={() => {
+            handlePageChange(page - 1);
+          }}
+          disabled={page <= 1}
+        >
+          Prev
+        </button>
+        {page}/ {totalPage}
+        <button
+          className="px-2 py-1 text-sm text-white bg-blue-400 rounded"
+          onClick={() => {
+            handlePageChange(page + 1);
+          }}
+          disabled={page + 1 >= totalPage}
+        >
+          Next
+        </button>
       </div>
     </>
   );
