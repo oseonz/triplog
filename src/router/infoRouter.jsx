ee29c8d810349ea7baccfb9082332c7550105234
Steave Inhun Ko <yoimono@naver.com>
2025-06-02 20:21:09 +0900

    이벤트상세연결

--- src/router/infoRouter.jsx
+++ src/router/infoRouter.jsx
@@ -2,6 +2,7 @@ import EventPage from "../pages/info/EventPage";
 import ArticlesPage from "../pages/info/ArticlesPage";
 import IndexPage from "../pages/info/IndexPage";
 import { Navigate } from "react-router-dom";
+import DetailPage from "../pages/search/DetailPage";
 
 const courseRouter = () => {
   return {
@@ -11,6 +12,7 @@ const courseRouter = () => {
       { path: "", element: <Navigate to="event" replace /> }, // 기본 리다이렉트
       { path: "event", element: <EventPage /> },
       { path: "article", element: <ArticlesPage /> },
+      { path: "detail/:contentid", element: <DetailPage /> },
     ],
   };
 };
